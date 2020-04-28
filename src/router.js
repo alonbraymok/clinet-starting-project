7b826c3f369ec5a79e9ca024a3bd574896b7e713
Alon Braymok <alon@ludus.ac>
2020-04-28 10:25:12 +0300

    test

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import App from "./App";
 
 const Router = () => {
+  console.log("df");
   return (
     <Switch>
       <Route exact path="/" component={App} />
